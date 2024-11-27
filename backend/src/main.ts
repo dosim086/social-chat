d022b8bfb75c3faa9f078f3ad7470ab88a39d00d
dosim086 <DKozhayev@spg.co.uk>
2024-11-27 18:46:05 +0500

    standard http response schema

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { FormatResponseInterceptor } from './common/interceptors/format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
 
   app.setGlobalPrefix('api/v1');
   app.enableCors();
